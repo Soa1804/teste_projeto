8b8f36d515fae2ee2a106c1eb5fabb5ee46c6158
David Soares <137106233+Soa1804@users.noreply.github.com>
2026-06-19 17:37:21 +0000

    concertando

--- js/supabase.js
+++ js/supabase.js
@@ -1,4 +1,4 @@
-import { createClient } from 'http://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
 export const supabaseUrl = 'https://uhflpbmxmzgkqwjpyhon.supabase.co'
 export const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVoZmxwYm14bXpna3F3anB5aG9uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg1OTI2ODUsImV4cCI6MjA5NDE2ODY4NX0.aZxJB5IiLQ7dwxppRB-W_lCh-cnx1TqH7Ng6jC_1oZk'
